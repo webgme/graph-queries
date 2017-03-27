7573d1cab17b8bb7382082e838e816299093b664
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-03-27 08:52:23 -0500

    Validate config

--- config/config.production.js
+++ config/config.production.js
@@ -5,11 +5,13 @@
 
 'use strict';
 
-var config = require('./config.default');
+var config = require('./config.default'),
+    validateConfig = require('webgme/config/validator');
 
 config.server.port = 80;
 config.authentication.enable = true;
 config.authentication.jwt.privateKey = path.join(__dirname, '..', '..', 'token_keys', 'private_key');
 config.authentication.jwt.publicKey = path.join(__dirname, '..', '..', 'token_keys', 'public_key');
 
+validateConfig(config);
 module.exports = config;
