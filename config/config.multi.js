4121a40009755700355ec69440b96762dba83957
pmeijer <patrik.meijer@vanderbilt.edu>
2017-04-19 16:11:32 +0000

    settings for multiple webgme backends

--- config/config.multi.js
+++ config/config.multi.js
@@ -14,5 +14,13 @@ config.authentication.enable = true;
 config.authentication.jwt.privateKey = path.join(__dirname, '..', '..', 'token_keys', 'private_key');
 config.authentication.jwt.publicKey = path.join(__dirname, '..', '..', 'token_keys', 'public_key');
 
+config.socketIO.adapter.type = 'redis';
+config.socketIO.adapter.options.uri = '127.0.0.1:6379';
+
+config.blob.fsDir = '../blob-local-storage';
+
+config.addOn.enable = true;
+config.addOn.workerUrl = 'http://127.0.0.1:8080';
+
 validateConfig(config);
 module.exports = config;
